0f9ace324c6ea2d842815f02a898abdf832ec3d2
ondreji <hbj0331@gmail.com>
2023-04-14 11:44:35 +0800

    Update main.go
    ignore arm64 kernel check

--- main.go
+++ main.go
@@ -30,10 +30,10 @@ func main() {
 		if kv < kernel.VersionCode(4, 18, 0) {
 			log.Fatalf("Linux/Android Kernel (x86_64) version %v is not supported. Need > 4.18 .", kv)
 		}
-	case "arm64":
-		if kv < kernel.VersionCode(5, 5, 0) {
-			log.Fatalf("Linux/Android Kernel (aarch64) version %v is not supported. Need > 5.5 .", kv)
-		}
+	//case "arm64":
+	//	if kv < kernel.VersionCode(5, 5, 0) {
+	//		log.Fatalf("Linux/Android Kernel (aarch64) version %v is not supported. Need > 5.5 .", kv)
+	//	}
 	default:
 		log.Fatalf("unsupported CPU arch:%v. ", runtime.GOARCH)
 	}
